59f5729e66b5b5574898524def33e7d66f374d37
Andrey Seresev <seresev-a@yandex.ru>
2022-07-22 15:38:56 +0300

    added model

--- scraper/models.py
+++ scraper/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
 
-class DeviceDescription(models.Model):
-    query = models.TextField
-    mttr = models.DecimalField
-    mtbf = models.DecimalField
-    failure_rate = models.DecimalField
-    failure_rate_in_storage_mode = models.DecimalField
-    storage_time = models.DecimalField
-    minimal_resource = models.DecimalField
-    gamma_percentage_resource = models.DecimalField
-    average_resource = models.DecimalField
-    average_lifetime = models.DecimalField
-    recovery_intensity = models.DecimalField
-    system_reliability = models.DecimalField
-    score = models.IntegerField
+class Device(models.Model):
+    name = models.CharField(max_length=255)
+    mttr = models.FloatField()
+    mtbf = models.FloatField()
+    failure_rate = models.FloatField()
+    failure_rate_in_storage_mode = models.FloatField()
+    storage_time = models.FloatField()
+    minimal_resource = models.FloatField()
+    gamma_percentage_resource = models.FloatField()
+    average_resource = models.FloatField()
+    average_lifetime = models.FloatField()
+    recovery_intensity = models.FloatField()
+    system_reliability = models.FloatField()
+    score = models.IntegerField()
+    link = models.TextField()
