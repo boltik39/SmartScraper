10308212ff54cfb54eb3c270435204d6c9545bac
Andrey Seresev <seresev-a@yandex.ru>
2022-10-28 14:55:41 +0300

    Deploy

--- SmartScraper/settings.py
+++ SmartScraper/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)3_n)12#^p12@kf7v3ifdr57y74mp-yho$8vxr!93^+(cv3bdm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
