1c2fd019f0cdb285d0279d01200654025872b199
Andrey Seresev <aaseresev@miem.hse.ru>
2023-01-10 19:19:42 +0300

    changing db

--- SmartScraper/settings.py
+++ SmartScraper/settings.py
@@ -82,16 +82,10 @@ WSGI_APPLICATION = 'SmartScraper.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        'NAME': 'd48q13ts47s3gm',
-        'USER': 'jpqojrvasxjuxd',
-        'PASSWORD': '67f7c5f54b78823f6c8d9c80b5691a77c88c82ce4da0f99a262273ae80338c33',
-        'HOST': 'ec2-99-81-68-240.eu-west-1.compute.amazonaws.com',
-        'PORT': '5432',
-        },
-}
+'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(os.path.join(BASE_DIR, 'db.sqlite3')),
+    }
 
 
 # Password validation
